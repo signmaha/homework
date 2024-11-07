fec3c8931d8e03ff2262258a73b61198b2d101ec
Мария <logacheva-33@mail.ru>
2024-11-07 09:30:40 +0300

    homework

---
+++ task.py
@@ -0,0 +1,33 @@
+
+grades = [[5, 3, 3, 5,4],[2, 2, 2, 3],[4, 5, 5, 2],[4, 4, 3],[5, 5, 5, 4, 5]]
+
+students = {'Johny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}   #какой командой упорядочитть в алфавитном порядке
+
+averagesscore= {}
+
+grades_1 = ((5+3+3+5+4)/5)
+grades_2 = ((2+2+2+3)/4)
+grades_3 = ((4+5+5+2)/4)
+grades_4 = ((4+4+3)/3)
+grades_5 = ((5+5+5+4+5)/5)
+print(grades_1, grades_2, grades_3, grades_4, grades_5)
+
+averagersscore = {'Aaron': grades_1, 'Bilbo': grades_2, 'Khendrik': grades_3,
+                  'Steve': grades_4, 'Jhony': grades_5 }
+print(averagersscore)
+
+
+
+
+
+
+
+
+#grades = [5, 3, 3, 5,4]
+#total = sum(grades)
+#print(total)
+
+#names = list(students)
+
+
+#averagesscore.update({'Aaron'[4]:sum(grades[1] /len(grades[1]})
