848f46c69370d6c4caca771d20eed8387ed89775
Мария <logacheva-33@mail.ru>
2024-11-09 00:19:51 +0300

    task corrected

--- task.py
+++ task.py
@@ -9,15 +9,19 @@ print(sorted(students))
 employee_grade = [[5, 3, 3, 5,4],[2, 2, 2, 3],[4, 5, 5, 2],[4, 4, 3],[5, 5, 5, 4, 5]]
 employee_student = ['Aaron', 'Bilbo', 'Johny', 'Khendrik', 'Steve']
 
-for student, grade in zip(employee_student, employee_grade):
-      print(student, grade)
+
+for student, grade in zip(students, grades):
+     averagesscore[student] = sum(grade)/ len(grade)
+
+print(averagesscore)
 
 
-grade_1 = ((5+3+3+5+4)/5)
-grade_2 = ((2+2+2+3)/4)
-grade_3 = ((4+5+5+2)/4)
-grade_4 = ((4+4+3)/3)
-grade_5 = ((5+5+5+4+5)/5)
+
+#grade_1 = ((5+3+3+5+4)/5)
+#grade_2 = ((2+2+2+3)/4)
+#grade_3 = ((4+5+5+2)/4)
+#grade_4 = ((4+4+3)/3)
+#grade_5 = ((5+5+5+4+5)/5)
 #print(grades_1, grades_2, grades_3, grades_4, grades_5)
 
 #for student, grade in zip(students, grades):
