3c09e4694d5c41cc3225ce5ab9c22bd63ca86c16
Мария <logacheva-33@mail.ru>
2024-11-08 11:32:47 +0300

    task corrected

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 home_work = 12
-print(home_work)
+print (home_work)
+
 time_of_work = 1.5
-print(time_of_work)
+print (time_of_work)
+
 name_course = 'Python'
-print(name_course)
+print (name_course)
+
 time_task = 1.5 / 12
-print(time_task)
+print (time_task)
