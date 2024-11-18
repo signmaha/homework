5a05cc8481e4e3fa56c8aa5a4c6668a7c4e60e3f
Мария <logacheva-33@mail.ru>
2024-11-19 00:06:28 +0300

    homework

--- tasks.py
+++ tasks.py
@@ -1,29 +1,11 @@
-#1st programm
-result = (9 ** 0,5) * 5
-print(result)
+# 1st programm
+print(9 ** 0, 5 * 5)
 
-#2nd program
- if 9.99 > 9.98 and 1000 != 1000.1:
-     print(True)
- else:
-     print(False)
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
 
-     #3nd program
-     result1 = 2 * 2 + 2
-     result2 = 2 * (2 + 2)
+# 3nd program
+print(2 * 2 + 2 == 2 * (2 + 2))
 
-     if result1 == result2:
-        print(True)
-     else:
-         print(False)
-
-         #4nd program
-         input_string = '123.456'
-
-         number_1 = float(input_string)
-         number_2 = number_1 * 10
-
-         integer_part = int(number_2)
-
-         result = integer_part % 10
-         print(result)
+ # 4nd program
+print(int(float('123.456')* 10)% 10)
