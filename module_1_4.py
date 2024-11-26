331ee79d2365332477ab5583eb1074ad70aca0d3
Мария <logacheva-33@mail.ru>
2024-11-26 22:55:09 +0300

    homework

--- module_1_4.py
+++ module_1_4.py
@@ -1,15 +1,8 @@
-my_string = input("Введите свое имя: ")
+my_string = input("Введите свое имя: ".replace(' ', ''))
+print(len(my_string))
 print(my_string.upper())
 print(my_string.lower())
 
-print(len(my_string))
-
-print('Введите свое имя'.replace(' ',''))
-
-my_string = "Введите своё имя"
 print(my_string[0])
 
-my_string = "Введите своё имя"
 print(my_string[-1])
-
-
